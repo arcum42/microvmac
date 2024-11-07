d8e2257f314ca3cc65c524c7dedaba35c2a87217
Shanoah Alkire <arcum42@gmail.com>
2024-11-07 15:16:31 -0800

    Consolidate dbglog.

--- src/UTIL/dbglog.h
+++ src/UTIL/dbglog.h
@@ -16,8 +16,22 @@
 
 #pragma once
 
-
 #include "spdlog/spdlog.h"
+#include "sys_dependencies.h"
+#include "UI/math.h"
+
+/* --- sending debugging info to file --- */
+
+#if dbglog_HAVE
+#define dbglog_bufsz PowOf2(dbglog_buflnsz)
+#define dbglog_open dbglog_open0
+
+void dbglog_writeCStr(char *s);
+void dbglog_writeReturn(void);
+void dbglog_writeHex(uimr x);
+void dbglog_writeNum(uimr x);
+void dbglog_writeMacChar(uint8_t x);
+#endif
 
 #if WantDisasm
 extern void dbglog_StartLine(void);
